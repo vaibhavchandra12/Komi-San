a78cdf36af2b37912b0c4b5ffb47d5236f16cc2f
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-04 13:33:49 +0530

    Update and rename nksama__init__.py to livvy/__init__.py

--- livvy/__init__.py
+++ livvy/__init__.py
@@ -6,7 +6,7 @@ bot = Client(
     api_id=os.environ.get('API_ID'),
     api_hash=os.environ['API_HASH'],
     bot_token=os.environ['BOT_TOKEN'],
-    plugins=dict(root=f"{__name__}/plugins")
+    plugins=dict(root=f"livvy/plugins")
 )
 
 
